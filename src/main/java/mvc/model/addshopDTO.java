d84a1818ff16b6624b88941f978910ebf3efcb33
Kim-Hyunil <jacku159@naver.com>
2022-05-23 20:03:18 +0900

    //현일 추가 java파일 커밋

---
+++ src/main/java/mvc/model/addshopDTO.java
@@ -0,0 +1,69 @@
+package mvc.model;
+
+public class addshopDTO {
+	private String name;
+	private String stime;
+	private String ftime;
+	private String phone;
+	private String area1;
+	private String area2;
+	private String category;
+	private String mtext;
+	private String mitext;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getStime() {
+		return stime;
+	}
+	public void setStime(String stime) {
+		this.stime = stime;
+	}
+	public String getFtime() {
+		return ftime;
+	}
+	public void setFtime(String ftime) {
+		this.ftime = ftime;
+	}
+	public String getArea1() {
+		return area1;
+	}
+	public void setArea1(String area1) {
+		this.area1 = area1;
+	}
+	public String getArea2() {
+		return area2;
+	}
+	public void setArea2(String area2) {
+		this.area2 = area2;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getMtext() {
+		return mtext;
+	}
+	public void setMtext(String mtext) {
+		this.mtext = mtext;
+	}
+	public String getMitext() {
+		return mitext;
+	}
+	public void setMitext(String mitext) {
+		this.mitext = mitext;
+	}
+}
